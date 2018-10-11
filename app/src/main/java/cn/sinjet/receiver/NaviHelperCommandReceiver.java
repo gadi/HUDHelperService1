e4d63d8ba1e3900550f1f78991553ebdb6c43853
Segev Alon <segevalon50@gmail.com>
2018-10-11 09:58:58 +0300

    get speed

--- app/src/main/java/cn/sinjet/receiver/NaviHelperCommandReceiver.java
+++ app/src/main/java/cn/sinjet/receiver/NaviHelperCommandReceiver.java
@@ -1,38 +1,96 @@
 package cn.sinjet.receiver;
 
+import android.app.ActivityManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
 import android.util.Log;
 
 import cn.sinjet.module.ToDevNaviCodec;
+import cn.sinjet.sinjetservice.SinjetService;
+
+import static android.content.Context.ACTIVITY_SERVICE;
+import static android.content.Context.LOCATION_SERVICE;
 
 
 public class NaviHelperCommandReceiver extends BroadcastReceiver {
+
     private ToDevNaviCodec mNaviCodec = new ToDevNaviCodec();
+    private LocationManager locationManager;
+    private int lastSpeed = 0;
+    String valueInstructionCode;
+    String valueDistance;
+    String[] parts;
+    Message lastMessage;
+    int distance;
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        Float f;
-        //Log.d("ssss", "NaviHelperCommandReceiver");
-        String valueDistance = intent.getStringExtra("Distance");
-        String valueInstructionCode = intent.getStringExtra("InstructionCode");
+
+        lastMessage = new Message();
+        locationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
+        valueDistance = intent.getStringExtra("Distance");
+        valueInstructionCode = intent.getStringExtra("InstructionCode");
         Log.d("ssss", "valueInstructionCode - " + valueInstructionCode);
 
-        String[] parts = valueDistance.split(" ");
+        parts = valueDistance.split(" ");
+
+//        if (getSpeed() != null) {
+//            lastSpeed = Math.round(getSpeed());
+//            Log.d("aaaa", "speed = " + lastSpeed);
+//        }
+//        mNaviCodec.sendLimitSpeed(110, 100);
 
 
         if (parts[0].contains(".")) {
-            int distance = Math.round(Float.valueOf(parts[0]) * 1000);
+            distance = Math.round(Float.valueOf(parts[0]) * 1000);
             Log.d("ssss", "is Float - " + distance);
+            lastMessage.setDistance(distance);
+            lastMessage.setImageCode(getImageCodeInstruction(valueInstructionCode));
             mNaviCodec.sendnaviInfo(getImageCodeInstruction(valueInstructionCode), distance);
+
         } else {
             int distance = Integer.valueOf(parts[0]);
             Log.d("ssss", "not Float - " + distance);
+            lastMessage.setDistance(distance);
+            lastMessage.setImageCode(getImageCodeInstruction(valueInstructionCode));
             mNaviCodec.sendnaviInfo(getImageCodeInstruction(valueInstructionCode), distance);
         }
-
     }
+
+
+    public class Message {
+        private int imageCode;
+        private int distance;
+
+        public Message() {
+
+        }
+
+        public Message(int imageCode, int distance) {
+            this.imageCode = imageCode;
+            this.distance = distance;
+        }
+
+        public int getImageCode() {
+            return imageCode;
+        }
+
+        public void setImageCode(int imageCode) {
+            this.imageCode = imageCode;
+        }
+
+        public int getDistance() {
+            return distance;
+        }
+
+        public void setDistance(int distance) {
+            this.distance = distance;
+        }
+    }
+
 
 
     public int getImageCodeInstruction(String InstructionCode) {
@@ -126,5 +184,59 @@ public class NaviHelperCommandReceiver extends BroadcastReceiver {
 
         }
         return 1;
+    }
+
+    public Float getSpeed() {
+
+        Location gpsLocation = null;
+
+        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) || locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+
+            if (locationManager != null) {
+                try {//first try to find location
+                    gpsLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+                    if (gpsLocation == null) {
+                        Log.d("aaaa", "gpsLocation is null");
+                    } else {
+                        Log.d("aaaa", String.valueOf(gpsLocation.getLatitude()));
+                        Log.d("aaaa", String.valueOf(gpsLocation.getLongitude()));
+                    }
+                } catch (SecurityException e) {
+                    Log.d("aaaa", "Error: getLastKnownLocation failed - " + e);
+
+                }
+
+                if (gpsLocation == null) {//second try to find location
+                    try {
+                        gpsLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (gpsLocation != null) {
+                            Log.d("aaaa", String.valueOf(gpsLocation.getLatitude()));
+                            Log.d("aaaa", String.valueOf(gpsLocation.getLongitude()));
+                        } else {
+                            Log.d("aaaa", "gpsLocation is null");
+                        }
+                    } catch (SecurityException e) {
+                        Log.d("aaaa", "Error: getLastKnownLocation failed 2");
+
+                    }
+                }
+
+            } else {
+                Log.d("aaaa", "locationManager is null!");
+            }
+
+        }
+        return gpsLocation.getSpeed();
+    }
+
+    private boolean isServiceRunning(Context context) {
+        ActivityManager manager = (ActivityManager) context.getSystemService(ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)){
+            if("cn.sinjet.sinjetservice".equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
